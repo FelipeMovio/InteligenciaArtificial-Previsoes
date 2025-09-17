241b79995249a04cca5cff5a0446c7a858fcdd37
FelipeMovio <felipelsmovio@gmail.com>
2025-09-17 20:15:18 -0300

    preparando a base de dados para a IA

--- projeto.py
+++ projeto.py
@@ -1,11 +1,29 @@
 # Criar um sistema de forma automatica que define se o cliente é seguro ou nao 
     # Score de credito do cliente 
-    # Boa
-    # Ok
-    # Ruim
+    # Boa - Good
+    # Ok - Standard
+    # Ruim - Poor
 
 # Passo 1 : importar base de dados 
 
 import pandas as pd
 
 tabela = pd.read_csv("./clientes.csv")
+
+# Passo 2 - tratamento de dados / prepara a base de dados para a IA
+
+print(tabela.info())
+# Ver informações da base de dados
+
+# LabelEncoder
+# Transformando valores na base de dados que sao objetos/textos em numeros 
+from sklearn.preprocessing import LabelEncoder
+
+codificador = LabelEncoder()
+
+tabela["profissao"] = codificador.fit_transform(tabela["profissao"])
+
+tabela["mix_credito"] = codificador.fit_transform(tabela["mix_credito"])
+
+tabela["comportamento_pagamento"] = codificador.fit_transform(tabela["comportamento_pagamento"])
+
